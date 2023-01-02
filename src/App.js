b8ed9bf63af83543bef85d77b7290c41e602cdfb
mlkmhmtbyk <biyikm16@itu.edu.tr>
2023-01-02 05:04:38 +0300

    getting first food with axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+const [food, setFood] = useState([]); 
+
+const getFood = async () => {
+  const response = await axios.get('https://localhost:5000/api/Foods/GetFood1');
+  setFood(response.data);
+  console.log("a");
+}
+
+useEffect(() => {
+  getFood();
+}, []);
+
+// const getFood = async () => {
+//   const response = await axios.get('https://localhost:5000/api/Foods/GetFood1');
+//   console.log(response);
+//   setFood(response.data);
+// }
+
+// useEffect(() => {
+//    axios.get('https://localhost:5000/api/Foods/GetFood1').then(response => {
+//     setFood(response.data);
+//     console.log(response.data);
+//   }, [])
+// })
+
+// useEffect(() => {
+//   getFood();
+//   console.log("1")
+// }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{food.foodName}</h1>
     </div>
   );
 }
